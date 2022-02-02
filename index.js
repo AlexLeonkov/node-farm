f32cefce1ed959bcb56379e0edf7353f349aff38
AlexLeonkov <aleonkov2001@gmail.com>
2022-02-02 13:40:20 +0300

    Project with node.js

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+//SERVER
+
+const tmpOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tmpCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tmpProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //OVERVIEW
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tmpCard, el))
+      .join("");
+
+    const output = tmpOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
+
+    res.end(output);
+
+    //PRODUCT
+  } else if (pathname === "/PRODUCT") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const product = dataObj[query.ID];
+    const output = replaceTemplate(tmpProduct, product);
+
+    res.end(output);
+
+    //API
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    //NOT FOUND
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+
+    res.end("<h1>page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
